bd5e7f4f75513a1941d6eb05b46597b252e968fe
金浩楠 <694196619@qq.com>
2021-06-08 00:09:51 +0800

    add two new components

---
+++ src/UI/Backpack.ts
@@ -0,0 +1,7 @@
+/**
+ * this is used for render a 2d canvas backpack
+ */
+
+export default class Backpack{
+    
+}
---
+++ src/UI/CharacterStats.ts
@@ -0,0 +1,9 @@
+/**
+ * this is a 2D UI used for display 2D State
+ * render to 2dCanvas and make it as a texture;
+ */
+
+export default class CharacterStatus{
+
+    
+}
